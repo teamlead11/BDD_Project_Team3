8a1e149a33479030cb08af17ba23d7a52912c2a4
AbiAppu <mail2abi1109@gmail.com>
2017-12-15 00:11:06 +0530

    Step definition of HomepageMiddle commit

---
+++ src/main/java/com/stepdefinition/HomePageMiddle.java
@@ -0,0 +1,66 @@
+package com.stepdefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HomePageMiddle {
+	
+	@Given("^I am on  Homepage$")
+	public void i_am_on_Homepage() throws Throwable {
+			    
+	}
+
+	@When("^I click on BookNow in featured hotels$")
+	public void i_click_on_BookNow_in_featured_hotels() throws Throwable {
+	    
+	}
+
+	@Then("^I verify the navigation to HotelsPage$")
+	public void i_verify_the_navigation_to_HotelsPage() throws Throwable {
+	    
+	}
+
+	@When("^I click on BookNow in featured tours$")
+	public void i_click_on_BookNow_in_featured_tours() throws Throwable {
+	    
+	}
+
+	@Then("^I verify the navigation to ToursPage$")
+	public void i_verify_the_navigation_to_ToursPage() throws Throwable {
+	    
+	}
+
+	@When("^I click on BookNow in featured cars$")
+	public void i_click_on_BookNow_in_featured_cars() throws Throwable {
+	    
+	}
+
+	@Then("^I verify the navigation to CarsPage$")
+	public void i_verify_the_navigation_to_CarsPage() throws Throwable {
+	    
+	}
+
+	@When("^I click on ViewMoreOffers$")
+	public void i_click_on_ViewMoreOffers() throws Throwable {
+	    
+	}
+
+	@Then("^I verify the navigation to offers$")
+	public void i_verify_the_navigation_to_offers() throws Throwable {
+	    
+	}
+
+	@When("^I click on ReadMore in Blog News$")
+	public void i_click_on_ReadMore_in_Blog_News() throws Throwable {
+	   
+	}
+
+	@Then("^I verify the navigation to BlogPage$")
+	public void i_verify_the_navigation_to_BlogPage() throws Throwable {
+	   
+	}
+
+
+
+}
